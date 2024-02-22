ef8e23b33c4ff25ff14312e20aaff61af583c28c
salcedokevin <salcedo.kevin.31@gmail.com>
2024-02-21 19:10:52 -0500

    Update main.py

--- 12-modulos/main.py
+++ 12-modulos/main.py
@@ -14,3 +14,35 @@ from mimodulo import *
 
 print(holaMundo("Kevin Salcedo"))
 print(calculadora(1, 3, True))
+
+# MODULO DE FECHAS
+import datetime
+
+print(datetime.date.today())
+
+fecha_completa = datetime.datetime.now()
+
+print(fecha_completa)
+print(fecha_completa.year)
+print(fecha_completa.month)
+print(fecha_completa.day)
+
+fecha_personalizada = fecha_completa.strftime("%d/%m/%Y , %H:%M:%S")
+print("Mi fecha personalizada: " + fecha_personalizada)
+
+print(datetime.datetime.now().timestamp())
+
+# MODULO MATEMATICA
+import math
+
+print("Raiz cuadrada de 10 es ", math.sqrt(10))
+
+print("Numero pi: ", float(math.pi))
+
+print("Redondear: ", math.ceil(6.52658))
+print("Redondear: ", math.floor(6.52658))
+
+# MODULO RANDOM
+import random
+
+print("Numero aleatorio entre 15 y 67: ", random.randint(15, 67))
