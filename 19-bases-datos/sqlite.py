394de529f46436bef1a2110354e1d022c32367ef
salcedokevin <salcedo.kevin.31@gmail.com>
2024-03-06 20:24:40 -0500

    a

--- 19-bases-datos/sqlite.py
+++ 19-bases-datos/sqlite.py
@@ -8,27 +8,50 @@ conexion = sqlite3.connect('./19-bases-datos\pruebas.db')
 cursor = conexion.cursor()
 
 # CREAR TABLA
-cursor.execute("CREATE TABLE IF NOT EXISTS PRODUCTOS("+
-"ID INTEGER PRIMARY KEY AUTOINCREMENT,"+
-"TITULO VARCHAR(255), "+
-"DESCRIPCION TEXT, "+
-"PRECIO INT(255)"+
-")")
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS PRODUCTOS(
+    "ID INTEGER PRIMARY KEY AUTOINCREMENT,
+    "TITULO VARCHAR(255),
+    "DESCRIPCION TEXT,
+    "PRECIO INT(255)
+);
+""")
 
 # GUARDAR CAMBIOS
 conexion.commit()
 
 # INSERTAR DATOS
+"""
 cursor.execute("INSERT INTO PRODUCTOS VALUES(NULL, 'SEGUNDO PRODUCTO', 'DESCRIPCION PRODUCTO', 550)")
+conexion.commit()
+"""
+
+# BORRAR REGISTROS
+cursor.execute("DELETE FROM PRODUCTOS")
+conexion.commit()
+
+# INSERTAR MUCHOS REGISTROS A LA VEZ
+productos = [
+    ("Ordenador portatil", "Buen pc", 700),
+    ("tablet", "15 pulgadas", 500),
+    ("PC", "gaaaa", 1800),
+]
+cursor.executemany("INSERT INTO PRODUCTOS VALUES (null, ?,?,?)", productos)
+conexion.commit()
+
+# ACTUALIZAR DATOS
+cursor.execute("UPDATE PRODUCTOS SET PRECIO = 1200 WHERE PRECIO = 500")
 conexion.commit()
 
 # LISTAR DATOS
-cursor.execute("SELECT * FROM PRODUCTOS")
+cursor.execute("SELECT * FROM PRODUCTOS WHERE PRECIO >= 500;")
 PRODUCTOS = cursor.fetchall()
 
 for elemento in PRODUCTOS:
+    print("ID: ", elemento[0])
     print("Titulo: ", elemento[1])
     print("Descripcion: ", elemento[2])
+    print("Precio: ", elemento[3])
     print("\n")
 
 cursor.execute("SELECT TITULO FROM PRODUCTOS")
